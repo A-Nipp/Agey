3b86a1b28234ed8e8576463652c75d971032e29e
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-09-15 11:41:19 -0400

    improved button appearance

--- Agey/View/ContentView.swift
+++ Agey/View/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var vm = AgeyViewModel()
+    let textFieldBackgroundColor: Color = Color(.sRGBLinear, red: 255, green: 255, blue: 255, opacity: 0.02)
     var body: some View {
         NavigationView {
             ZStack {
@@ -17,28 +18,42 @@ struct ContentView: View {
                 VStack {
                     Spacer()
                     Text("Someone named **\(vm.displayName)** is about")
-                        
+                    
                     
                     Text("\(vm.displayAge)")
                         .font(.system(size: 80))
                         .fontWeight(.heavy)
                         .padding(.horizontal, 15)
-
+                    
                     Text("years old!")
                     
                     Spacer()
                     
-                    TextField("Enter name", text: $vm.currentTextEntry)
-                        .padding(.horizontal, 5)
-                        .padding(.vertical)
-                        .background(.white, in: RoundedRectangle(cornerRadius: 10))
-                        .padding()
-                    Button("Fetch age") {
+                    HStack {
+                        Spacer()
+                        
+                        TextField("Enter name", text: $vm.currentTextEntry)
+                            .multilineTextAlignment(.center)
+                            .fixedSize()
+                            .padding(.horizontal, 5)
+                            .padding(.vertical)
+                            .background(textFieldBackgroundColor, in: RoundedRectangle(cornerRadius: 10))
+                            .padding(.vertical, 10)
+                            .padding(.horizontal, 40)
+                        Spacer()
+                    }
+                    Button(action: {
                         Task {
                             await vm.fetchAge()
                         }
-                    }
-                    .buttonStyle(.borderedProminent)
+                    }, label: {
+                        Text("Fetch Age")
+                            .font(.title3)
+                            .foregroundColor(.primary)
+                            .padding(10)
+                            .padding(.horizontal, 15)
+                            .background(Color.blue, in: Capsule())
+                    })
                     Spacer()
                     
                 }
