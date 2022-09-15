dbbe0d8b3133f7cdc7ce0c5612173facb9971f10
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-09-14 21:20:17 -0400

    Initial Commit

---
+++ Agey/AgeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgeyApp.swift
+//  Agey
+//
+//  Created by AlecNipp on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct AgeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
